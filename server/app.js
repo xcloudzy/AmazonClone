5b5003b60c9ebafb7751c2bac83817f9b1bed28e
xcoudzy <amit4.pagal@gmail.com>
2024-07-11 22:11:32 +0530

    2

--- server/app.js
+++ server/app.js
@@ -17,6 +17,17 @@ app.use(router);
 
 const port = 8005;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://cloudamazon.vercel.app");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
 app.get("*", (req, res, next) => {
   res.status(200).json({
     message: "Connected to vercel app",
