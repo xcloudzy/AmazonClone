e897b2cdcdc173a91f895df1f4c462071b3bcc8f
xcoudzy <amit4.pagal@gmail.com>
2024-07-11 22:46:46 +0530

    2

--- server/app.js
+++ server/app.js
@@ -12,21 +12,16 @@ const router = require("./routes/router");
 
 app.use(express.json());
 app.use(cookieParser(""));
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://cloudamazon.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 app.use(router);
 
 const port = 8005;
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://cloudamazon.vercel.app");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  next();
-});
 
 app.get("*", (req, res, next) => {
   res.status(200).json({
