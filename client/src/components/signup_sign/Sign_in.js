3172ccd44a4f0fb1c37b0c58a6a74b02e827ef3b
xcoudzy <amit4.pagal@gmail.com>
2024-07-12 12:47:03 +0530

    2

--- client/src/components/signup_sign/Sign_in.js
+++ client/src/components/signup_sign/Sign_in.js
@@ -34,6 +34,7 @@ const Sign_in = () => {
         headers: {
           "Content-Type": "application/json",
         },
+        credentials: "include",
         body: JSON.stringify({
           email,
           password,
--- server/db/conn.js
+++ server/db/conn.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 const DB = process.env.DATABASE;
 
 mongoose
-  .connect(DB)
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("Database Connected");
   })
